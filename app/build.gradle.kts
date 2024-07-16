074d614625ce164fa0124d8ed27fcedefd10cdc6
Ромчик <roman-chernega@yandex.ru>
2024-07-17 00:47:30 +0300

    add implementations and ViewModel class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation(libs.volley)
+    implementation(libs.picasso)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.play.services.location)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
---
+++ app/src/main/java/com/example/newweatherapp/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.newweatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val lifeDataCurrent = MutableLiveData<String>()
+
+
+
+}
